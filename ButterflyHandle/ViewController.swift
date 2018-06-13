f991562322ac06c36ecc25fb61d8816ac8f3e428
usagimaru <usagimaru@ahirumaru.local>
2018-06-14 02:15:26 +0900

    add project

---
+++ ButterflyHandle/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ButterflyHandle
+//
+//  Created by usagimaru on 2018.06.14.
+//  Copyright © 2018年 usagimaru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	@IBOutlet weak var butterflyHandle1: ButterflyHandleView!
+	@IBOutlet weak var butterflyHandle2: ButterflyHandleView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		butterflyHandle2.direction = .bottom
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	@IBAction func touchDown(_ sender: Any) {
+		butterflyHandle1.spread(animated: true)
+		butterflyHandle2.spread(animated: true)
+	}
+	
+	@IBAction func touchUpOrCancelled(_ sender: Any) {
+		butterflyHandle1.flap(animated: true)
+		butterflyHandle2.flap(animated: true)
+	}
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ButterflyHandle
+
 A handle indicator like iOS Music app.
+
+<img width=310 src="demo.gif">
+
+<img width=375 src="musicapp.jpg">
+
+# Class
+
+- `ButterflyHandleView.swift`
+
+This is a subclass of UIView, the view has only two control methods:
+
+```
+func spread(animated: Bool)
+func flap(animated: Bool)
+```
+
+And the property for direction of arrow:
+
+```
+/// `top` or `bottom`
+var direction: direction
+```
